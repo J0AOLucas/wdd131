c0fb5082b815d3d8369cd6374bcc23ac8678eb1e
João Lucas da Silva <lucasrpg.silva@gmail.com>
2025-09-28 00:40:19 -0600

    fix: adding just a few layout improvements

--- mission/script.js
+++ mission/script.js
@@ -1,5 +1,6 @@
 let selectorElement = document.querySelector('select');
 let logo = document.querySelector('img');
+let subtitle = document.getElementById('subtitle')
 let body = document.body;
 
 selectorElement.addEventListener('change', changeTheme);
@@ -11,6 +12,7 @@ function changeTheme() {
 
   if (current == 'dark') {
     logo.src = './src/images/byui-logo-white.png'
+    subtitle.style.color = '#00bfff'
   } else {
     logo.src = './src/images/byui-logo-blue.webp'
   }
