56c891e375dd2a67a8907e730dba778d4b7c3d79
João Lucas da Silva <lucasrpg.silva@gmail.com>
2025-10-11 23:58:30 -0600

    feat: adding the modal in the code

---
+++ coolpics/modal.js
@@ -0,0 +1,39 @@
+
+const gallery = document.querySelector('.images-group');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Elementos do menu mobile
+const menuButton = document.querySelector('h2');
+const mobileMenu = document.getElementById('mobile-menu');
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    console.log(e.target);
+    const img = e.target;
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    
+    modalImage.src = src;
+    modalImage.alt = alt;
+
+    modal.showModal();
+    
+}
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+
+
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('show');
+});
+          
