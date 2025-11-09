f3e9c5c30c9dfea53198dbd3fd720e57c96fbbcc
João Lucas da Silva <lucasrpg.silva@gmail.com>
2025-11-08 23:43:22 -0700

    feat: working with social media tags

--- charactercard/script.js
+++ charactercard/script.js
@@ -1,17 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   var levelText = document.getElementById("level");
   var healthText = document.getElementById("health");
-  let level = parseInt(levelText.textContent)
-  let health = parseInt(healthText.textContent)
+  let level = parseInt(levelText.textContent);
+  let health = parseInt(healthText.textContent);
   let attackButton = document.getElementById("attack");
-  let levelUp = document.getElementById("levelUp");
+  let levelUpButton = document.getElementById("levelUp");
   
   function Attack() {
-    healthText.textContent = health - 20
-    // I can remove this one
-    health -= 20
+    health -= 20;
+    healthText.textContent = health;
+
+    if(health <= 0)
+    {
+      setTimeout(() => {
+        alert("Character Died!");
+      }, 2);
+    }
   }
 
+  function LevelUp() {
+    level += 1;
+    levelText.textContent = level;
+  }
 
-  attackButton.addEventListener('click', Attack)
+  attackButton.addEventListener('click', Attack);
+  levelUpButton.addEventListener('click', LevelUp);
 })
