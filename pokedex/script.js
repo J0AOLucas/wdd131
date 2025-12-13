38544e84e781de46c348e046c9bd0a2af31fb962
JoaoLucas <lucasrpg.silva@gmail.com>
2025-12-13 01:19:06 -0700

    feat: sending dynamic data to modal

--- pokedex/script.js
+++ pokedex/script.js
@@ -2,7 +2,26 @@ document.addEventListener('DOMContentLoaded', () =>
 {
     const cardsGroup = document.getElementById('cards');
     const modal = document.getElementById('modal');
-
+    const colors = {
+        normal: '#A8A77A',
+        fire: '#EE8130',
+        water: '#6390F0',
+        electric: '#F7D02C',
+        grass: '#7AC74C',
+        ice: '#96D9D6',
+        fighting: '#C22E28',
+        poison: '#A33EA1',
+        ground: '#E2BF65',
+        flying: '#A98FF3',
+        psychic: '#F95587',
+        bug: '#A6B91A',
+        rock: '#B6A136',
+        ghost: '#735797',
+        dragon: '#6F35FC',
+        dark: '#705746',
+        steel: '#B7B7CE',
+        fairy: '#D685AD',
+};
 
     fetch('https://pokeapi.co/api/v2/pokemon')
     .then(response => response.json())
@@ -11,6 +30,7 @@ document.addEventListener('DOMContentLoaded', () =>
 
         for (const element of results) {
             let generalDetails = await GetGeneralDetails(element['url']);
+
             const id = generalDetails['id'];
             const name = generalDetails['name'];
             const types = generalDetails['types'];
@@ -28,8 +48,9 @@ document.addEventListener('DOMContentLoaded', () =>
                 </div>
             `;
 
-            card.addEventListener('click', () => {
-                modal.style.display = 'flex';
+            card.addEventListener('click', async () => {
+                await UpdateModal(generalDetails)
+                openModal();
             });
 
             closeButtom.addEventListener('click', () => {
@@ -46,9 +67,43 @@ document.addEventListener('DOMContentLoaded', () =>
         }
     })
 
-    function ShowModal(pokemonData) {
-        
+    function openModal() {
+        modal.style.display = 'flex';
     }
+
+    async function UpdateModal(generalDetails) {
+        let id = generalDetails['id']
+        let hp = generalDetails['base_experience']
+        let height = generalDetails['height']
+        let weight = generalDetails['weight']
+        let habilities = GetHabilities(generalDetails['abilities']);
+        let photo = GetPhoto(id);
+        let modalPhoto = document.getElementById('pokemon-modal-photo');
+        let pokemonName = document.getElementById('modal-pokemon-name');
+        let name = generalDetails['name'];
+        let typeColor = generalDetails['types'][0]['type']['name']
+        let color = colors[typeColor]
+        let modalContent = document.getElementById('modal-content');
+        let hpElement = document.getElementById('hp');
+        let heightElement = document.getElementById('height');
+        let weightElement = document.getElementById('weight');
+        let habilitiesElement = document.getElementById('habilities');
+
+        modalContent.style.backgroundColor = color;
+        modalPhoto.src = photo;
+        modalPhoto.style.width = '400px';
+        pokemonName.textContent = CapitalizeWord(name);
+        hpElement.textContent = hp;
+        heightElement.textContent = height;
+        weightElement.textContent = weight;
+        habilitiesElement.textContent = habilities;
+    }
+
+    // async function GetColor(id) {
+    //     let response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+    //     let data = await response.json();
+    //     return data['color']['name']
+    // }
 
     function GetTypeImage(types)
     {
@@ -65,6 +120,12 @@ document.addEventListener('DOMContentLoaded', () =>
     {
         let image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
         return image;
+    }
+
+    function GetHabilities(habilities) {
+        return habilities.map(a => 
+            CapitalizeWord(a['ability']['name'])
+        ).join(', ');
     }
 
     async function GetGeneralDetails(link)
