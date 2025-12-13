3e7d86bb3adddbf776e3d947c5a0715b72d2b139
JoaoLucas <lucasrpg.silva@gmail.com>
2025-12-12 20:45:45 -0700

    feat: cards created

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,8 +1,53 @@
-document.addEventListener("DOMContentLoaded", () =>
+document.addEventListener('DOMContentLoaded', () =>
 {
+    let cards = document.getElementById('cards');
     fetch('https://pokeapi.co/api/v2/pokemon')
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
+    .then(async data => {
+        let results = data['results']
+
+        for(const element of results) {
+            let generalDetails = await GetGeneralDetails(element['url'])
+            const id = generalDetails['id'];
+            const name = generalDetails['name'];
+            const types = generalDetails['types'];
+            const pokemonImage = GetPhoto(id);
+
+            let card = `
+                <div class='card'>
+                    <h1 class='pokemon-id'>ID: ${id}</h1>
+                    <img class='pokemonImage' src='${pokemonImage}' alt='Pokemon Image'>
+                    <p class='pokemon-name'>${CapitalizeWord(name)}</p>
+                    <div class='image-types'>
+                        ${GetTypeImage(types)}
+                    </div>
+                </div>
+                `
+            cards.innerHTML += card;
+        };
     })
+
+    function GetTypeImage(types)
+    {
+        return types.map(element => 
+            `<img class='type-image' src='./src/images/types/${element['type']['name']}.png' alt='power type'>`
+        ).join('');
+    }
+
+    function CapitalizeWord(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }
+
+    function GetPhoto(id)
+    {
+        let image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+        return image;
+    }
+
+    async function GetGeneralDetails(link)
+    {
+        const response = await fetch(link);
+        const data = await response.json();
+        return data;
+    }
 })
